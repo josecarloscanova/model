a5ce0fd25d03718e30cbabcef19e07e93795ccf4
Darth Vader <jose.carlos.canova@gmail.com>
2016-10-04 12:34:39 -0300

    Added arquillian sample to test wildfly configuration

---
+++ src/test/java/org/hubotek/model/GreeterTest.java
@@ -0,0 +1,32 @@
+package org.hubotek.model;
+
+import javax.inject.Inject;
+
+import org.hubotek.Greeter;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class GreeterTest {
+	
+	@Inject
+	Greeter greeter;
+
+	@Deployment
+	public static JavaArchive createDeployment() {
+		return ShrinkWrap.create(JavaArchive.class)
+				.addClass(Greeter.class)
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@Test
+	public void should_create_greeting() {
+	    greeter.greet(System.out, "Earthling");
+	}
+	
+}
